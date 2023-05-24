1929a1b4496db7fe370f619a5f88bed3ce543bc6
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-05-25 01:04:35 +0530

    first commit

---
+++ app/src/main/java/com/example/vitbhopal/hostel_selection.java
@@ -0,0 +1,14 @@
+package com.example.vitbhopal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hostel_selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hostel_selection);
+    }
+}
