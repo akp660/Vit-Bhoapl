8e2adb888bb62ef099e381eddee3c1afb558ddc8
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-05-25 01:52:54 +0530

    first commit

---
+++ app/src/main/java/com/example/vitbhopal/dev_profile.java
@@ -0,0 +1,14 @@
+package com.example.vitbhopal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dev_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dev_profile);
+    }
+}
