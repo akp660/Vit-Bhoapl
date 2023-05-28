6e6a50dcee60722f038a40794fdecbc7878aa3ea
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-05-28 10:30:41 +0530

    first commit

--- app/src/main/java/com/example/vitbhopal/loading_screen.java
+++ app/src/main/java/com/example/vitbhopal/loading_screen.java
@@ -2,7 +2,9 @@ package com.example.vitbhopal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class loading_screen extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class loading_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        Intent iHome = new Intent(loading_screen.this,MainActivity.class);
+
+        new Handler().postDelayed(() -> startActivity(iHome),3000);
+
     }
 }
