3ab8113a9ed391598d497eee578cbc7297010535
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-06-01 10:19:14 +0530

    changed background colour of dev page

--- app/src/main/java/com/example/vitbhopal/dev_profile.java
+++ app/src/main/java/com/example/vitbhopal/dev_profile.java
@@ -2,7 +2,9 @@ package com.example.vitbhopal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class dev_profile extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class dev_profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dev_profile);
+
+        ImageView imageView = findViewById(R.id.home);
+        imageView.setOnClickListener(view -> home());
+
+    }
+
+    public void home(){
+        Intent intent = new Intent(dev_profile.this,MainActivity.class);
+        startActivity(intent);
     }
 }
