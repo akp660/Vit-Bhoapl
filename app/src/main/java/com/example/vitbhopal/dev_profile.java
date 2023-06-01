f70da849c43978fe388547fb4412d6a093c6d5cd
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-06-01 10:47:28 +0530

    added on click listner on image in dev page

--- app/src/main/java/com/example/vitbhopal/dev_profile.java
+++ app/src/main/java/com/example/vitbhopal/dev_profile.java
@@ -3,10 +3,16 @@ package com.example.vitbhopal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 public class dev_profile extends AppCompatActivity {
+
+    ImageView gmail;
+    ImageView github;
+    ImageView linkedin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,34 @@ public class dev_profile extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.home);
         imageView.setOnClickListener(view -> home());
+
+        gmail = findViewById(R.id.gmail);
+        github = findViewById(R.id.github);
+        linkedin = findViewById(R.id.linkedin);
+
+
+        gmail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                gotoUrl("https://vtop.vitbhopal.ac.in/vtop/initialProcess");
+            }
+            private void gotoUrl(String s) {
+                Uri uri = Uri.parse(s);
+                startActivity(new Intent(Intent.ACTION_VIEW,uri));
+            }
+        });
+
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoUrl("https://vtop.vitbhopal.ac.in/vtop/initialProcess");
+            }
+
+            private void gotoUrl(String s) {
+                gotoUrl("https://vtop.vitbhopal.ac.in/vtop/initialProcess");
+            }
+        });
+
 
     }
 
