483726476b499702530ec95606357219d9fd6550
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-06-05 10:44:28 +0530

    connected mess menu screen with hostel selection  screen

--- app/src/main/java/com/example/vitbhopal/hostel_selection.java
+++ app/src/main/java/com/example/vitbhopal/hostel_selection.java
@@ -2,7 +2,10 @@ package com.example.vitbhopal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class hostel_selection extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class hostel_selection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hostel_selection);
+
+        LottieAnimationView lottieAnimationView = findViewById(R.id.lottieAnimationView3);
+        lottieAnimationView.setOnClickListener(view -> vtop());
+
+    }
+
+    public void vtop(){
+        Intent intent = new Intent(hostel_selection.this, mess_menu.class);
+        startActivity(intent);
     }
 }
