e4832062fa21a5484e66f2088908b076aa52ca4e
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-06-13 15:39:42 +0530

    added animation in refresh button

--- app/src/main/java/com/example/vitbhopal/mess_menu.java
+++ app/src/main/java/com/example/vitbhopal/mess_menu.java
@@ -3,12 +3,30 @@ package com.example.vitbhopal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class mess_menu extends AppCompatActivity {
-
+    ImageView refresh_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mess_menu);
+
+        refresh_button=findViewById(R.id.refresh_button);
+
+        refresh_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                RotateAnimation rotateAnimation=new RotateAnimation(0,360,RotateAnimation.RELATIVE_TO_SELF,.5F);
+
+                rotateAnimation.setDuration(1000);
+                refresh_button.startAnimation(rotateAnimation);
+
+            }
+        });
+
     }
 }
