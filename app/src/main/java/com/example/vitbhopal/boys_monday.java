21a29dad44913919c2d0cd191abb8c3a85130f66
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-06-25 17:50:38 +0530

    added boys monday mess menu layout

---
+++ app/src/main/java/com/example/vitbhopal/boys_monday.java
@@ -0,0 +1,14 @@
+package com.example.vitbhopal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class boys_monday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boys_monday);
+    }
+}
