350be747973481a73c6c91be03d9dc35cce111c1
Abhijeet Kumar Pandey <abhijeetpandey.dhn@gmail.com>
2023-06-26 13:48:57 +0530

    created new layout

--- app/src/main/java/com/example/vitbhopal/under_belly.java
+++ app/src/main/java/com/example/vitbhopal/under_belly.java
@@ -1,14 +1,6 @@
 package com.example.vitbhopal;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
-import android.os.Bundle;
-
-public class under_belly extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_under_belly);
-    }
+public class under_belly extends Activity {
 }
